a19e3e8df7bff4cf4919c988f1b704329c22f1ac
javaman2017 <tommooney2017@outlook.com>
2017-08-01 19:00:03 +0100

    Calculator.java interface with add as the first operation

---
+++ tdd-samples/src/main/java/practicalunittesting/interfaces/Calculator.java
@@ -0,0 +1,22 @@
+package practicalunittesting.interfaces;
+
+import java.math.BigDecimal;
+
+
+/**
+ *  The <code>Calculator</code> class provides a set of 
+ *  binary arithmetic operations for working with BigDecimals
+ */
+public interface Calculator {
+	
+	
+	/**
+	 * Adds two BigDecimals together
+	 * 
+	 * @param arg1
+	 * @param arg2
+	 * @return arg1 + arg2 as a BigDecimal
+	 */
+	BigDecimal add(BigDecimal arg1, BigDecimal arg2);
+
+}
