76a49f18358b9af7fb2736756445fa45eb0c0142
javaman2017 <tommooney2017@outlook.com>
2017-08-01 18:47:07 +0100

    Class Money.java holds a monetary amount in a specific concurrency. The class is accompanied by a Unit test class

---
+++ tdd-samples/src/main/java/practicalunittesting/Money.java
@@ -0,0 +1,19 @@
+package practicalunittesting;
+
+public class Money {
+	private final int amount;
+	private final String currency;
+	
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+}
---
+++ tdd-samples/src/test/java/practicalunittesting/MoneyTest.java
@@ -0,0 +1,17 @@
+package practicalunittesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void constructorShouldSetAmountAndCurrency() {
+		Money money = new Money(10,"USD");
+		
+		assertEquals(10,money.getAmount());
+		assertEquals("USD",money.getCurrency());
+	}
+
+}
