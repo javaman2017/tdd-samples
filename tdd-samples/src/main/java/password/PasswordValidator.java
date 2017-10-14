72c13da9880ba4d1984370630401ad13b61a5aa5
mr-x <mr-x@mr-x-computer>
2017-10-14 18:12:35 +0100

    Refactored to use a simple regex for 3 or more digits in a password

--- tdd-samples/src/main/java/password/PasswordValidator.java
+++ tdd-samples/src/main/java/password/PasswordValidator.java
@@ -1,25 +1,15 @@
 package password;
 
+import java.util.regex.Pattern;
+
 public class PasswordValidator {
 
     public void validate(String string) {
        if(string.length() < 8)
            throw new IllegalArgumentException("Passwords must contain at least 8 characters");
-       
-       int digitCount = 0;
-       int specialCharCount = 0;
-       for(int i = 0; i < string.length(); i++)
-           if (Character.isDigit(string.charAt(i)))
-               digitCount++;
-           
-       if (digitCount < 3)
+                  
+       if (!Pattern.matches("[0-9]{3,}", string))
            throw new IllegalArgumentException("Passwords must contain at least 3 digits");
-       
-       
-       
-       
-       
-        
     }
 
 }
