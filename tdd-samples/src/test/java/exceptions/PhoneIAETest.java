bbae295063c32b1d40030a6212273d7275408a9f
javaman2017 <thomascmooney@hotmail.co.uk>
2017-10-31 18:43:39 +0000

    Forgot fails in test

--- tdd-samples/src/test/java/exceptions/PhoneIAETest.java
+++ tdd-samples/src/test/java/exceptions/PhoneIAETest.java
@@ -3,6 +3,7 @@ package exceptions;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class PhoneIAETest {
     private Phone phone = new Phone();
@@ -11,6 +12,7 @@ public class PhoneIAETest {
     public void shouldThrowIAEForEmptyNumber(){
         try{
             phone.setNumber("");
+            fail("Should have thrown an illegalArgumentException");
         } catch (IllegalArgumentException iae){
             assertEquals("number cannot be null or empty",iae.getMessage());
         }
@@ -20,6 +22,7 @@ public class PhoneIAETest {
     public void shouldThrowIAEForPlusPrefixedNumber(){
         try{
             phone.setNumber("+123456789");
+            fail("Should have thrown an illegalArgumentException");
         } catch (IllegalArgumentException iae){
             assertEquals("numbers cannot start with + number:[+123456789]",iae.getMessage());
         }
