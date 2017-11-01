cdcba3afc854a1dff07198384244b8299b35f3fa
javaman2017 <thomascmooney@hotmail.co.uk>
2017-11-01 07:42:19 +0000

    Example test class for RequestHandler

---
+++ tdd-samples/src/test/java/exceptions/RequestHandlerIVRTest.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class RequestHandlerIVRTest {
+    @Test
+    public void shouldThrowInvalidRequestExceptionForInvalidRequest() throws InvalidRequestException {
+        RequestProcessor requestProcessor = mock(RequestProcessor.class);
+        RequestHandler sut = new RequestHandler(requestProcessor);
+
+        Request request = null;
+
+        catchException(sut).handle(request);
+
+        assertTrue("should have thrown an instance of InvalidRequestException",
+                caughtException() instanceof InvalidRequestException);
+
+        Mockito.verifyZeroInteractions(requestProcessor);
+    }
+}
